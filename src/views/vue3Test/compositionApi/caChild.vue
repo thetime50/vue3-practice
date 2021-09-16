e1541b1fcc510df29e896386e7701b4d26c1bdcf
luoxin <thetime_lx@qq.com>
2021-09-16 19:01:49 +0800

    back

--- src/views/vue3Test/compositionApi/caChild.vue
+++ src/views/vue3Test/compositionApi/caChild.vue
@@ -3,6 +3,7 @@
         <p>
             ca-child
         </p>
+        <h4>ref test</h4>
         <el-form >
             <el-form-item label="strRef">{{strRef}} <input v-model="strRef"/> </el-form-item>
             <el-divider />
@@ -20,14 +21,41 @@
                 <button type="button" @click="refObjAttrRandom">refObjAttrRandom</button>
             </el-form-item>
         </el-form>
+
+
+        <h4>reactive test</h4>
+        <el-form >
+            <el-form-item label="objReat">{{objReat}} </el-form-item>
+            <el-form-item label="objReat.str">{{objReat.str}} <input v-model="objReat.str"/> </el-form-item>
+            <el-form-item label="objReatStrRef">{{objReatStrRef}} <!-- <input v-model="objReatStrRef"/> --> </el-form-item>
+            <el-form-item label="objReatValRef">{{objReatValRef}} <!-- <input v-model="objReatValRef"/> --> </el-form-item>
+            <el-form-item label="">
+                <button type="button" @click="objReatValRefRandom">objReatValRefRandom</button> 响应式会同步更新到objReat里的属性
+            </el-form-item>
+
+        </el-form>
+        <h4>setup render test</h4>
+        <setupRender />
+
+        <h4>watch test</h4>
+        <el-form>
+          <el-form-item label="watchStrRef">{{watchStrRef}}<input v-model="watchStrRef"/></el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script>
 /* message */
-import { ref } from 'vue';
+import {
+  ref, reactive, toRefs,
+  onMounted, onBeforeUnmount, onUnmounted, onRenderTracked, onRenderTriggered,
+  watch,
+} from 'vue';
+import { ElMessage } from 'element-plus';
+import setupRender from './setupRender.vue';
 
 function refSetup(props, context) { // eslint-disable-line
+  /* ref */
   const strRef = ref('hello');
 
   console.log('* strRef');
@@ -57,21 +85,80 @@ function refSetup(props, context) { // eslint-disable-line
     refObj.value.str = `refObj attr random ${val}`;
     refObj.value.val = val;
   }
+
+  /* reactive */
+  const objReat = reactive({
+    str: 'this is reactive obj',
+    val: 0,
+  });
+  const { str: objReatStrRef, val: objReatValRef } = toRefs(objReat);
+
+  function objReatValRefRandom() {
+    const val = parseInt(Math.random() * 100);
+    objReatValRef.value = val;
+  }
+  // reactive ref 互相嵌套时会互相更新
+  // 用ref 初始化reactive 或者赋值作为reactive属性时会自动解包
+
   return {
     strRef,
     refObj,
     refObjRandom,
     refObjAttrRandom,
+
+    objReat,
+    objReatStrRef,
+    objReatValRef,
+    objReatValRefRandom,
+  };
+}
+
+function hookSetup(props, context) { // eslint-disable-line
+  onMounted((...args) => {
+    console.log('onMounted', ...args);
+  });
+  onBeforeUnmount((...args) => {
+    console.log('onBeforeUnmount', ...args);
+  });
+  onUnmounted((...args) => {
+    console.log('onUnmounted', ...args);
+  });
+  onRenderTracked((...args) => {
+    console.log('onRenderTracked', ...args);
+  });
+  onRenderTriggered((...args) => {
+    console.log('onRenderTriggered', ...args);
+  });
+  return {};
+}
+
+function watchSetup(props, context) { // eslint-disable-line
+  const watchStrRef = ref('aaa');
+  watch(watchStrRef, (after, before) => {  // eslint-disable-line
+    ElMessage({
+      message: `watchStrRef change ${after}`,
+      type: 'success',
+    });
+  });
+  return {
+    watchStrRef,
   };
 }
 
 export default {
   name: 'ca-child',
+  components: {
+    setupRender,
+  },
   setup(props, context) {
     console.log('composition api test');
     const refRes = refSetup(props, context);
+    const hookRes = hookSetup(props, context);
+    const watchRes = watchSetup(props, context);
     return {
       ...refRes,
+      ...hookRes,
+      ...watchRes,
     };
   },
 };
---
+++ src/views/vue3Test/compositionApi/setupRender.vue
@@ -0,0 +1,31 @@
+<!--
+<template>
+    <div class="component-setup-render">
+
+    </div>
+</template>
+ -->
+<script>
+/* message */
+import { ref, h } from 'vue'; // eslint-disable-line
+
+export default {
+  name: 'setup-render',
+  setup(props, context) { // eslint-disable-line
+    const numRef = ref(3);
+    const render = () => (
+        <div class="component-setup-render">
+            {numRef.value}
+            { (<input v-model={numRef.value}/>) }
+        </div>
+    );
+    return render;
+  },
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    .component-setup-render{
+
+    }
+</style>
